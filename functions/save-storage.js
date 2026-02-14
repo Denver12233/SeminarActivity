b2fa7f979d2a0a914bcde7b43d84e9e9780f2e09
Denver12233 <tandingandenverm@gmail.com>
2026-02-14 11:34:46 +0800

    functions/add-entry.js functions/renderEntry.js functions/save-storage.js functions/savelocal.js index-style.css  index.html

---
+++ functions/save-storage.js
@@ -0,0 +1,10 @@
+ const list = document.getElementById('guest-list');
+
+        // Load data on startup
+        window.onload = function() {
+            const savedData = localStorage.getItem('guestbook_entries');
+            if (savedData) {
+                const entries = JSON.parse(savedData);
+                entries.forEach(entry => renderEntry(entry.name, entry.learn));
+            }
+        };
